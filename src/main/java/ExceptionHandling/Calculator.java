85fdf1d0403552b3f8af7c9ab544280df414085a
Jay Milnamow <jaymilnamow@Zipcoder29s-MacBook-Pro.local>
2016-05-18 21:03:08 -0400

    Using Exceptions

---
+++ src/main/java/ExceptionHandling/Calculator.java
@@ -0,0 +1,31 @@
+package ExceptionHandling;
+
+/**
+ * Created by jaymilnamow on 5/18/16.
+ */
+public class Calculator {
+
+    public static double add(double a,double b){
+
+        return a+b;
+    }
+
+    public static double subtract(double a,double b){
+
+        return a-b;
+    }
+
+    public static double multiply(double a, double b){
+
+        return a*b;
+    }
+
+    public static double divide(double a, double b) throws Exception{
+        if(b == 0) {
+            throw new Exception();
+        }
+            return a / b;
+        }
+
+    }
+}
